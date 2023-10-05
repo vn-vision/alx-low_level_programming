8b3ab0d96aa7e3c8ae922977fcf49167dd3fd442
nv-vision <vctrngang@gmail.com>
2023-10-05 22:26:16 +0300

    mind white spaces in C

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,13 @@
 #include <stdio.h>
+
 /**
 * main - Entry point 
 * display text using printf 
 * Return: always 0 (success) 
 */
-int main(void) {
+
+int main(void)
+{
 	printf("with proper grammar, but the outcome is a piece of art,");
 	printf("\n");
 	return (0);
