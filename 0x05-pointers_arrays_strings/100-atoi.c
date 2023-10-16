c97bf885e015ba91df3b9b381ce49b03bd69e081
vn-Vision <vctrngang@gmail.com>
2023-10-16 22:41:33 +0300

    C -Pointers, Strings, Arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -6,22 +6,30 @@
  * Return: integer converted
  */
 
-int _atoi(char *s)
-{
-	int sign = 1, resp = 0, firstNum, i;
+int _atoi(char *s) {
+    int result = 0;
+    int sign = 1; // To keep track of the sign, default is positive
 
-	for (firstNum = 0; !(s[firstNum] >= 48 && s[firstNum] <= 57); firstNum++)
-	{
-		if (s[firstNum] == '-')
-		{
-			sign *= -1;
+    // Skip leading white spaces
+    while (*s == ' ' || (*s >= 9 && *s <= 13))
+        s++;
 
-		}
-	}
-	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
-	{
-		resp *= 10;
-		resp += (s[i] - 48);
-	}
-	return (sign * resp);
+    // Check for the sign
+    if (*s == '-' || *s == '+') {
+        if (*s == '-')
+            sign = -1;
+        s++;
+    }
+
+    // Convert the string to an integer
+    while (*s >= '0' && *s <= '9') {
+        // Check for integer overflow
+        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (*s - '0') > 7)) {
+            return (sign == 1) ? INT_MAX : INT_MIN;
+        }
+        result = result * 10 + (*s - '0');
+        s++;
+    }
+
+    return result * sign;
 }
