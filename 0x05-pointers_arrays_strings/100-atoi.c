895d4d03505d2fb712f5abe8c95ff2c7a1c35d53
vn-Vision <vctrngang@gmail.com>
2023-10-16 22:52:26 +0300

    C -Pointers, Strings, Arrays

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,35 +1,36 @@
 #include "main.h"
 
 /**
- * _atoi - convert a string to an integer.
- * @s: char type string
- * Return: integer converted
- */
+* _atoi - convert a string to an integer.
+* @s: char type string
+* Return: integer converted
+*/
 
-int _atoi(char *s) {
-    int result = 0;
-    int sign = 1; // To keep track of the sign, default is positive
+int _atoi(char *s)
+{
+	int result = 0;
+	int sign = 1;
 
-    // Skip leading white spaces
-    while (*s == ' ' || (*s >= 9 && *s <= 13))
-        s++;
+	while (*s == ' ' || (*s >= 9 && *s <= 13))
+		s++;
 
-    // Check for the sign
-    if (*s == '-' || *s == '+') {
-        if (*s == '-')
-            sign = -1;
-        s++;
-    }
+	if (*s == '-' || *s == '+')
+	{
+		if (*s == '-')
+		sign = -1;
+		s++;
+	}
 
-    // Convert the string to an integer
-    while (*s >= '0' && *s <= '9') {
-        // Check for integer overflow
-        if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (*s - '0') > 7)) {
-            return (sign == 1) ? INT_MAX : INT_MIN;
-        }
-        result = result * 10 + (*s - '0');
-        s++;
-    }
+	while (*s >= '0' && *s <= '9')
+	{
 
-    return result * sign;
+		if (result > INT_MAX / 10 || (result == INT_MAX / 10 && (*s - '0') > 7))
+		{
+			return (sign == 1) ? INT_MAX : INT_MIN;
+		}
+		result = result * 10 + (*s - '0');
+		s++;
+	}
+
+	return result * sign;
 }
