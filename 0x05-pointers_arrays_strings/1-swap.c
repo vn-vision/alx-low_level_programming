8461482c4283d8b1500a530755a7a70403b21be0
vn-Vision <vctrngang@gmail.com>
2023-10-16 13:09:15 +0300

    C -Pointers, Strings, Arrays

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -10,6 +10,8 @@
 
 void swap_int(int *a, int *b)
 {
-	*a = b;
-	*b = a;
+	int temp = *a;
+	*a = *b;
+	*b = temp;
 }
+
--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -8,7 +8,7 @@
 *
 */
 
-int _strlen(char *s);
+int _strlen(char *s)
 {
 	strlen(*s);
 }
