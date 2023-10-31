c221a2280406241cafec24c7b05f9a0e8b1bf832
vn-Vision <vctrngang@gmail.com>
2023-10-31 18:41:59 +0300

    Malloc_Free

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,13 +14,15 @@
 
 char *str_concat(char *s1, char *s2)
 {
-	if (s1 == NULL) s1 = "";
-	if (s2 == NULL) s2 = "";
-
 	char  *combo;
 	unsigned int i, j;
 
 	combo = malloc(strlen(s1) + strlen(s2) + 1);
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
 	if (combo != NULL)
 	{
