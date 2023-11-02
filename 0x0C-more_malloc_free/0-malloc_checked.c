760bf48424709ac2c63fead86e4fae0ecdf8d20d
vn-Vision <vctrngang@gmail.com>
2023-11-02 22:14:17 +0300

    Malloc_Free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+* malloc_checked - returns pointer
+* description: to memory
+* @b: the usigned bit
+* Return: 98 on termination
+*
+*/
+
+void *malloc_checked(unsigned int b)
+{
+	unsigned int *db = malloc(sizeof(unsigned int) * b);
+
+	if (db == NULL)
+	{
+		exit(98);
+	}
+
+	return db;
+}
