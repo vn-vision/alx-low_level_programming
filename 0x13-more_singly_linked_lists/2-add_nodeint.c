0f63fb06437b745228042219cfdeb0e81a70eeae
vn-vision <vctrngang@gmail.com>
2023-11-21 22:09:51 +0300

    0x13_more_singly_linked_lists

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -9,22 +9,22 @@
 * NULL if it failed.
 */
 
+
 listint_t *add_nodeint(listint_t **head, const int n)
 {
-	listint_t *new_node = malloc(sizeof(listint_t));
+	listint_t *new_node = NULL;
 
+	new_node = malloc(sizeof(listint_t));
 	if (new_node == NULL)
 		return (NULL);
-
-	if (head && *head)
+	new_node->n = n;
+	new_node->next = NULL;
+	if (*head == NULL)
 	{
-		new_node->n = n;
-		new_node->next = *head;
 		*head = new_node;
-		printf("%d", new_node->n);
-
 		return (*head);
 	}
-
-	return (NULL);
+	new_node->next = *head;
+	*head = new_node;
+	return (*head);
 }
