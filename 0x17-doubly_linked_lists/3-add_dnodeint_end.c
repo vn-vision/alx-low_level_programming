54f9ea0d5268bfd9ba960a0ec95629792cca6d92
vn-vision <vctrngang@gmail.com>
2024-01-11 18:45:15 -0800

    Add int at the beginning

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -14,22 +14,24 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 	dlistint_t *new, *temp;
 
 	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return (NULL);
 
 	new->n = n;
+	new->next = NULL;
 	temp = *head;
 
 	if (*head == NULL)
 	{
 		new->next = NULL;
 		new->prev = NULL;
-		*head = new;
 	}
 	else
 	{
 		while (temp->next != NULL)
 			temp = temp->next;
+
 		temp->next = new;
-		new->next = NULL;
 		new->prev = temp;
 	}
 	return (new);
