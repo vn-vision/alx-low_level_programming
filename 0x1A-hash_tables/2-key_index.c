20d48f6cafcc45dbd38b97c16c7dd36a0f1c3d6c
vn-vision <vctrngang@gmail.com>
2024-02-02 15:42:21 +0300

    Hash tables djb2 index

---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,13 @@
+#include "hash_tables.h"
+
+/**
+ * key_index - functions gives you index of a key
+ * @key: the key
+ * @size: size of the hash table
+ * Return: index of the key/value pair
+ */
+
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	return (hash_djb2(key) % size);
+}
