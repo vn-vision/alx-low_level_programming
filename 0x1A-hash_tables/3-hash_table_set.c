3f490542958e9cfbaabad916b6841c7d8062c8ff
vn-vision <vctrngang@gmail.com>
2024-02-02 22:03:04 +0300

    Hashed table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,17 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - adds element to hash table
+ * @ht: this is the hash table
+ * @:key: this is the key part
+ * @value: the value to part
+ * Return: 1 success, 0 otherwise
+ */
+
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	add element to to table ht
+	key cannot be an empty string
+	value must be duplicates, value can be empty
+	in case of collision add thr new node at the beginning.
+}
