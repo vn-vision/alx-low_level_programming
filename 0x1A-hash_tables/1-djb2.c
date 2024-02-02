bbd18a523c704d5c749d298ba96cad5411d615a3
vn-vision <vctrngang@gmail.com>
2024-02-02 14:39:46 +0300

    Hash tables djb2

---
+++ 0x1A-hash_tables/1-djb2.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * hash_djb2 - a hash function implementing
+ * the djb2 algorithm
+ * @str: the string 
+ * Return: the long int
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+	unsigned long int hash;
+	int c;
+
+	hash = 5381;
+	
+	while((c = *str++))
+	{
+		hash = ((hash << 5) + c;)
+	}
+
+	return (hash);
+}
