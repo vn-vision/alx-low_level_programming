7dfc7adf4c64d601551bbc677f06cd9efa36c85b
vn-vision <vctrngang@gmail.com>
2024-02-02 14:22:18 +0300

    Hash tables

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -19,8 +19,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (table == NULL)
 		return (NULL);
 
-	table->array = malloc(sizeof(hash_table_t) * size);
-	
+	table->array = malloc(sizeof(hash_node_t) * size);
+
 	if (table->array == NULL)
 	{
 		free(table);
@@ -33,4 +33,4 @@ hash_table_t *hash_table_create(unsigned long int size)
 		table->array[i] = NULL;
 	}
 	return (table);
-}	
+}
