46e6747764cf253a08fc34a6b78144128ea49103
vn-vision <vctrngang@gmail.com>
2024-02-02 13:54:54 +0300

    Hash tables

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,36 @@
+#include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ * hash_table_create - creates hash table
+ * @size: the size of array
+ * Return: returns a pointer to the hash table
+ */
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	unsigned long int i;
+	hash_table_t *table;
+
+	table = malloc(sizeof(hash_table_t));
+
+	if (table == NULL)
+		return (NULL);
+
+	table->array = malloc(sizeof(hash_table_t) * size);
+	
+	if (table->array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+
+	table->size = size;
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}	
