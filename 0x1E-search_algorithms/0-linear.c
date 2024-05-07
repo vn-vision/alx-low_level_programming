c3c8978a27df65fbf521f34d44505c1fcfad0ade
kZilla <kzilla@vn-z.zilla>
2024-05-07 11:29:51 +0300

    Nimepata shida

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -23,7 +23,7 @@ int linear_search(int *array, size_t size, int value)
 		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
 		if (array[i] == value)
 		{
-			k = array[i];
+			k = i;
 			break;
 		}
 	}
